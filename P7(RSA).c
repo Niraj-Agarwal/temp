920a9ed232c05d8dcb1dd02f2d230012f2d39a5d
NIRAJ AGARWAL <109128838+Niraj-Agarwal@users.noreply.github.com>
2024-06-26 08:36:12 +0530

    Update P7(RSA).c

--- P7(RSA).c
+++ P7(RSA).c
@@ -41,3 +41,45 @@ long long exponential(int base,int expo,int mod)
     return res;
     
 }
+int main() {
+    int p , q , e , m;
+    
+    printf("Enter the value of p: ");
+    scanf("%d" , &p);
+    
+    printf("Enter the value of q: ");
+    scanf("%d" , &q);
+    
+    printf("Enter the value of e: ");
+    scanf("%d" , &e);
+    
+    printf("Enter the message: ");
+    scanf("%d" , &m);
+    
+    int n = p * q;
+    int phi = (p-1)*(q-1);
+    
+    while(e < phi){
+        if(gcd(e , phi) == 1){
+            break;
+        }
+        else{
+            e++;
+        }
+    }
+    
+    int d = modinverse(e , phi);
+    
+    if(d == -1){
+        printf("No Modulo found inverse ");
+        return -1;
+    }
+    
+    long long c = exponential(m , e , n);
+    printf("Encrypted data is: %lld\n " , c);
+    
+    long long m1 = exponential(c , d , n);
+    printf("Decrypted data is: %lld\n " , m1);
+
+    return 0;
+}
